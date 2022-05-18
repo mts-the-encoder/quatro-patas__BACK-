51b0bbc3a03191ed8f95b8cbf00854d68f8da670
zmtheus <matheus.nsantos@bandtec.com.br>
2022-05-18 11:22:08 -0300

    removendo pra poder fazer do 0 bonitinho

--- src/app/Controller/CompanyController.js
+++
@@ -1,9 +0,0 @@
-class CompanyController {
-
-    index(req, res) {
-        console.log(req.body)
-    }
-
-}
-
-module.exports = new CompanyController();
--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-var PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`App listen on port: ${PORT}`);
-});
