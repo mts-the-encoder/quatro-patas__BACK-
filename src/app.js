a437c608845fe78310954c686c58721967e6c2ad
zmtheus <matheus.nsantos@bandtec.com.br>
2022-05-19 13:37:03 -0300

    My man....Mongo connection. Greatness. ���� Next up: Postgree/w.Docker.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//|-/ DbConnect - Mongo
+const dbUser = 'mts';
+const dbPasswd = 'urubu100';
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPasswd}@quatropatas.zjhon.mongodb.net/Data`)
+
 
 //|-/ Routes
 const indexRoute = require('./routes/index-route'); 
