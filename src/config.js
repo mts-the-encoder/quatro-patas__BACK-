5e3cd335d810b3d022258a302c6715a88986b95f
zmtheus <matheus.nsantos@bandtec.com.br>
2022-05-26 12:14:42 -0300

    azure storage

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ const dbPasswd = 'urubu100';
 
 module.exports = {
     connectionString: `mongodb+srv://${dbUser}:${dbPasswd}@quatropatas.zjhon.mongodb.net/Data`,
-    containerConnectionString: 'CONNECTION STRING'
+    containerConnectionString: ''
 }
--- src/controllers/pets-controller.js
+++ src/controllers/pets-controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const repository = require('../repositories/pet-repository');
+const azure = require('azure-storage');
+const guid = require('guid');
+const config = require('../config');
 
 exports.get = async (req, res, next) => {
     try {
@@ -49,11 +52,41 @@ exports.getByFav = async (req, res, next) => {
 
 exports.post = async(req, res, next) => {
     try {
-        await repository.create(req.body);
+
+        const blobSvc = azure.createBlobService(config.containerConnectionString);
+
+
+        let filename = guid.raw().toString() + '.jpg';
+        let rawdata = req.body.image;
+        let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        let type = matches[1];
+        let buffer = new Buffer(matches[2], 'base64');
+
+        // Salva a imagem
+        await blobSvc.createBlockBlobFromText('quatro-patas', filename, buffer, {
+            contentType: type
+        }, function (error, result, response) {
+            if (error) {
+                filename = 'default-product.png'
+            }
+        });
+
+        await repository.create({
+            name: req.body.name,
+            gender: req.body.gender,
+            species: req.body.species,
+            breed: req.body.breed,
+            age: req.body.age,
+            city: req.body.city,
+            favorite: req.body.favorite,
+            status: req.body.status,
+            image: 'https://mts4.blob.core.windows.net/quatro-patas/' + filename
+        });
         res.status(201).send({
             message: 'PEt cadastrado com sucesso!'
     });
     } catch (e) {
+        console.log(e);
         res.status(500).send({
             message: 'Falha ao processar sua informação'
         });
