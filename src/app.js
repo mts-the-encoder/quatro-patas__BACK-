9b137f93b8d54368d48d894353e192bee3dedc72
zmtheus <matheus.nsantos@bandtec.com.br>
2022-05-30 07:45:28 -0300

    CORS

--- src/app.js
+++ src/app.js
@@ -15,15 +15,24 @@ mongoose.connect(config.connectionString);
 const Pet = require('./models/pet');
 const Company = require('./models/company');
 
-
 //|-/ Routes
 const indexRoute = require('./routes/index-route'); 
 const petsRoute = require('./routes/pets-route'); 
 const companyRoute = require('./routes/company-route'); 
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+
+//|-/ CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/pets', petsRoute);
