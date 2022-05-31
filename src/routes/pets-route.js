b62db873ebf5e5e9bee8d926736ec9be689126e5
zmtheus <matheus.nsantos@bandtec.com.br>
2022-05-31 14:11:13 -0300

    swagger structure

--- src/routes/pets-route.js
+++ src/routes/pets-route.js
@@ -4,6 +4,25 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/pets-controller'); 
 
+/**
+ * @swagger 
+ *  components:
+ *   schemas:
+ *     Pets:
+ *       type: object
+ *       required:
+ *         - name
+ *       properties:
+ *         id:
+ *           type: string
+ *         name:
+ *           type: string
+ *       example:
+ *         id: d5fE_asz
+ *         name: The New Turing Omnibus
+ * 
+ */
+
 router.get('/', controller.get);
 router.get('/:name', controller.getByName);
 router.get('/species/:species', controller.getBySpecie);
@@ -13,3 +32,4 @@ router.put('/:id', controller.put);
 router.delete('/:id', controller.delete);
 
 module.exports = router;
+
